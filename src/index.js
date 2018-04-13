53b6243ff7a2214115f01ef748cc517a954d58d1
xaositectt <tamas.varga.420@gmail.com>
2018-04-14 01:47:40 +0200

    created responsive test layout, for 1080x1920 smartphone

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './App.css';
+import './style/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
